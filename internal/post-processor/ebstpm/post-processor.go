c4ef77b838d42a06cf3d6762a98ba723fe655b11
coding-ia <137450628+coding-ia@users.noreply.github.com>
2024-04-24 14:25:10 -0700

    Produce only a single image to a snapshot (#5)

--- internal/post-processor/ebstpm/post-processor.go
+++ internal/post-processor/ebstpm/post-processor.go
@@ -77,6 +77,7 @@ func (p *PostProcessor) Configure(raws ...interface{}) error {
 }
 
 func (p *PostProcessor) PostProcess(ctx context.Context, ui packer.Ui, artifact packer.Artifact) (a packer.Artifact, keep bool, forceOverride bool, err error) {
+	session, err := p.config.Session()
 	generatedData := artifact.State("generated_data")
 	if generatedData == nil {
 		// Make sure it's not a nil map so we can assign to it later.
@@ -92,30 +93,50 @@ func (p *PostProcessor) PostProcess(ctx context.Context, ui packer.Ui, artifact 
 	}
 
 	ui.Say("Creating secureboot images...")
-	processArtifact(artifact.Id(), p.config, ui)
+	amis := processArtifact(artifact.Id(), p.config, ui)
+
+	if len(amis) > 0 {
+		emptyState := make(map[string]interface{})
+		newArtifact := &awscommon.Artifact{
+			Amis:           amis,
+			BuilderIdValue: BuilderId,
+			StateData:      emptyState,
+			Session:        session,
+		}
+		err := artifact.Destroy()
+		if err != nil {
+			ui.Error(fmt.Sprintf("Error: %s", err))
+		}
+		return newArtifact, true, false, nil
+	}
 
 	return artifact, true, true, nil
 }
 
-func processArtifact(id string, config Config, ui packer.Ui) {
+func processArtifact(id string, config Config, ui packer.Ui) map[string]string {
+	images := make(map[string]string)
 	artifacts := strings.Split(id, ",")
 	for _, artifact := range artifacts {
 		parts := strings.Split(artifact, ":")
 		if len(parts) == 2 {
 			region := parts[0]
 			amiID := parts[1]
-			err := createTPM(region, amiID, config)
+			imageId, err := createTPM(region, amiID, config, ui)
 			if err != nil {
 				ui.Error(fmt.Sprintf("Error creating image: %s", err))
 			}
+			if imageId != nil {
+				images[region] = aws.StringValue(imageId)
+			}
 		}
 	}
+	return images
 }
 
-func createTPM(region string, amiID string, config Config) error {
+func createTPM(region string, amiID string, config Config, ui packer.Ui) (*string, error) {
 	session, err := config.Session()
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	ec2Svc := ec2.New(session, &aws.Config{
@@ -127,11 +148,11 @@ func createTPM(region string, amiID string, config Config) error {
 	}
 	describeOutput, err := ec2Svc.DescribeImages(describeInput)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	if len(describeOutput.Images) != 1 {
-		return errors.New("multiple images found, expected 1")
+		return nil, errors.New("multiple images found, expected 1")
 	}
 
 	image := describeOutput.Images[0]
@@ -143,7 +164,7 @@ func createTPM(region string, amiID string, config Config) error {
 
 	describeAttributeOutput, err := ec2Svc.DescribeImageAttribute(&describeAttributeInput)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	mappings := cloneBlockDeviceMappings(image.BlockDeviceMappings)
@@ -163,7 +184,7 @@ func createTPM(region string, amiID string, config Config) error {
 
 	result, err := ec2Svc.RegisterImage(input)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	imageID := aws.StringValue(result.ImageId)
 	log.Printf("Registered new AMI ID: %s", imageID)
@@ -177,7 +198,7 @@ func createTPM(region string, amiID string, config Config) error {
 		}
 		_, err = ec2Svc.ModifyImageAttribute(launchPermissionInput)
 		if err != nil {
-			return err
+			return nil, err
 		}
 		log.Printf("Copied permissions from %s to %s", aws.StringValue(image.ImageId), imageID)
 	}
@@ -185,7 +206,7 @@ func createTPM(region string, amiID string, config Config) error {
 	if image.DeprecationTime != nil {
 		deprecationTime, err := time.Parse("2006-01-02T15:04:05Z07:00", aws.StringValue(image.DeprecationTime))
 		if err != nil {
-			return err
+			return nil, err
 		}
 		deprecationInput := &ec2.EnableImageDeprecationInput{
 			ImageId:     result.ImageId,
@@ -193,11 +214,19 @@ func createTPM(region string, amiID string, config Config) error {
 		}
 		_, err = ec2Svc.EnableImageDeprecation(deprecationInput)
 		if err != nil {
-			return err
+			return nil, err
 		}
 	}
 
-	return nil
+	deregisterInput := &ec2.DeregisterImageInput{
+		ImageId: image.ImageId,
+	}
+	_, err = ec2Svc.DeregisterImage(deregisterInput)
+	if err == nil {
+		ui.Say(fmt.Sprintf("Deregistered AMI %s at %s", aws.StringValue(image.ImageId), region))
+	}
+
+	return result.ImageId, nil
 }
 
 func cloneBlockDeviceMappings(blockMappings []*ec2.BlockDeviceMapping) []*ec2.BlockDeviceMapping {
